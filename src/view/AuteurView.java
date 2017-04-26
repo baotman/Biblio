62f1a52be526ec4d0c7b9af9469e730cf6c376c8
farah <farah@farah-PC>
2017-04-26 09:50:41 +0100

    init project

--- src/view/AuteurView.java
+++ src/view/AuteurView.java
@@ -113,7 +113,7 @@ public class AuteurView extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 343, Short.MAX_VALUE)
         );
 
         pack();
